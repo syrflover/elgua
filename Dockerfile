245bd19c1d2198ddd14efe319d8865903d9ce4ca
syr <user@syrflover.co>
2025-10-06 13:39:39 +0900

    add docker builder

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,28 @@
-FROM ubuntu:jammy
+FROM rust:latest as builder
+
+RUN apt-get update && apt-get upgrade -y
+RUN apt-get install -y cmake
+RUN apt-get install -y libopus-dev
+
+WORKDIR /usr/src/elgua
+COPY . .
+
+RUN cargo install --path .
+
+FROM ubuntu:noble
 
 RUN sed -i.bak -re \
     "s/([a-z]{2}.)?archive.ubuntu.com|security.ubuntu.com/mirror.kakao.com/g" \
     /etc/apt/sources.list
 
-RUN apt-get update && apt-get upgrade -y && \
-    apt-get install -y ca-certificates ffmpeg python3
+RUN apt-get update && apt-get upgrade -y
+RUN apt-get install -y ca-certificates ffmpeg python3
 
 ARG BINARY_FILE
 ARG CFG_FILE
 
-COPY $BINARY_FILE /elgua
+# COPY $BINARY_FILE /elgua
+COPY --from=builder /usr/local/cargo/bin/elgua /elgua
 COPY $CFG_FILE /cfg.json
 
 ENTRYPOINT [ "/elgua" ]
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-MODE=$1
+# MODE=$1
 
 err="$(kubectl create namespace bots 2>&1)"
 if ! (grep -q "AlreadyExists" <<< "$err"); then
@@ -6,25 +6,27 @@ if ! (grep -q "AlreadyExists" <<< "$err"); then
     exit 1
 fi
 
-if [ "$1" = "prod" ]; then
-    BIN="target/x86_64-unknown-linux-musl/release/elgua"
+# if [ "$1" = "prod" ]; then
+#     BIN="target/x86_64-unknown-linux-musl/release/elgua"
 
-    cargo build --release --target=x86_64-unknown-linux-musl
-else
-    BIN="target/x86_64-unknown-linux-musl/debug/elgua"
+#     CMAKE_x86_64_unknown_linux_gnu=scripts/opus-cmake \
+#     cargo build --release --target=x86_64-unknown-linux-musl
+# else
+#     BIN="target/x86_64-unknown-linux-musl/debug/elgua"
 
-    cargo build --target=x86_64-unknown-linux-musl
-fi
+#     CMAKE_x86_64_unknown_linux_gnu=scripts/opus-cmake \
+#     cargo build --target=x86_64-unknown-linux-musl
+# fi
 
-if [ $? -ne 0 ]; then
-    exit 1
-fi
+# if [ $? -ne 0 ]; then
+#     exit 1
+# fi
 
-chmod +x $BIN
+# chmod +x $BIN
 
 docker build \
+    --progress auto \
     --platform linux/amd64 \
-    --build-arg BINARY_FILE="$BIN" \
     --build-arg CFG_FILE="cfg.json" \
     --tag "192.168.1.21:32000/elgua:latest" \
     --load .
